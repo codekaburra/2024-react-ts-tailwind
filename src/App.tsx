63ba5d9f3ef7ebf4f1c57c6f0a7d975f4b983751
EM <eve.proj.fun@gmail.com>
2024-08-04 06:32:04 +0800

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className='py-18 h-screen'>
+      <div className="block rounded-lg bg-white p-6 text-surface shadow-secondary-1 dark:bg-black dark:text-white">
+        <h5 className="mb-2 text-xl font-medium leading-tight">Card title</h5>
+        <p className="mb-4 text-base">
+          Testing Dark Mode Here
         </p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
+
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media", // add this line
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
